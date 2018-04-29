1c59a8d748dc3385e3eddc4bc022f4711e33ee62
Dhruva <dkarkada@gmail.com>
2018-04-28 19:36:07 -0500

    file write

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::error::Error;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
 fn main() {
     println!("Hello, world!");
+    let path = Path::new("lorem_ipsum.txt");
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}",
+                       	    path.display(),
+                            why.description()),
+        Ok(file) => file,
+    };
+    gen_data(&mut file, path);
+}
+
+fn gen_data(file: &mut File, path: &Path){
+    let content = ".data\n\
+                    \t\targc_: .quad 0\n\
+                    \t\tFormat: .byte '%%', 'l', 'u', 10, 0\n\
+                    \t\tFuncTable: .quad 0\n\
+                    \t\tFuncCall: .quad 0\n";
+    match file.write_all(content.as_bytes()) {
+        Err(why) => {
+            panic!("couldn't write to {:?}: {}", path,
+                                               why.description())
+        },
+        Ok(_) => println!("successfully wrote to {:?}", path),
+    }
 }
