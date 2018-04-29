ff3c5abf881b7d830489fc9c97266ce4c6894b2e
Michael Yu <m.yu8088@gmail.com>
2018-04-29 01:17:15 -0500

    tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+mod tokenizer;
+use tokenizer::Tokenizer;
+
 fn main() {
-    println!("Hello, world!");
+	let args : Vec<String> = env::args().collect();
+	let (program, output_file) = read_file(&args);
+	let mut tokenizer = Tokenizer::new(program);
+	tokenizer.start();
+	// tokenizer.test('\u{fe0f}');
+}
+
+fn read_file(args: &[String]) -> (String, String) {
+	let input_file = &args[1];
+	let output_file = &args[2];
+    
+    let mut f = File::open(input_file).expect("File not found.");
+    let mut program = String::new();
+    f.read_to_string(&mut program).expect("Reading file failed.");
+
+    (program, output_file.to_string())
 }
---
+++ src/tokenizer/mod.rs
@@ -0,0 +1,160 @@
+mod tokenizer_util;
+use std::char;
+use self::tokenizer_util::is_emoji;
+use self::tokenizer_util::is_keycap;
+use self::tokenizer_util::get_keycap_val;
+
+mod token_types;
+use self::token_types::TokenType;
+
+#[derive(Debug)]
+pub struct Token {
+    kind: TokenType,
+    value_int: u64,
+    value_str: String
+}
+
+#[derive(Debug, PartialEq)]
+enum TokenizerState {
+    DEFINING,
+    NONE
+}
+
+pub struct Tokenizer {
+    program: Vec<char>,
+    state: TokenizerState,
+    pos: usize
+}
+
+impl Tokenizer {
+    pub fn new(prog: String) -> Tokenizer {
+        Tokenizer {
+            program: prog.chars().collect(),
+            state: TokenizerState::NONE,
+            pos: 0
+        }
+    }
+
+    pub fn start(&mut self) {
+        let mut tkn = Token {
+            kind: TokenType::NONE,
+            value_int: 0,
+            value_str: String::from("")
+        };
+
+        println!("{:?}\n {}", &self.program, self.program.len());
+
+        while tkn.kind != TokenType::END {
+            tkn = self.get_token();
+            println!("{:?}, state: {:?}, pos: {}, actual char: {:?}", tkn, self.state, self.pos, self.program[self.pos] as u8);
+        }
+    }
+
+    pub fn get_token(&mut self) -> Token {
+        let mut tkn = Token {
+            kind: TokenType::NONE,
+            value_int: 0,
+            value_str: String::from("")
+        };
+
+        let mut pos = self.pos;
+        pos = self.fast_forward_whitespace(pos);
+
+        if pos < self.program.len() {
+            let prog = &self.program;
+            match prog[pos] {
+                '❌' => tkn.kind = TokenType::ELSE,
+                '❓' => tkn.kind = TokenType::IF,
+                '🌛' => tkn.kind = TokenType::RPAREN,
+                '🌜' => tkn.kind = TokenType::LPAREN,
+                '🎲' => tkn.kind = TokenType::RAND,
+                '🚫' => tkn.kind = TokenType::NOT,
+                '⬅' => tkn.kind = TokenType::EQ,
+                '↔' => tkn.kind = TokenType::EQEQ,
+                '✖' => tkn.kind = TokenType::MUL,
+                '➗' => tkn.kind = TokenType::DIV,
+                '➕' => tkn.kind = TokenType::PLUS,
+                '➖' => tkn.kind = TokenType::MINUS,
+                '🔀' => tkn.kind = TokenType::SWAP,
+                '🆕' => tkn.kind = TokenType::NEW,
+                '👍' => tkn.kind = TokenType::TRUE,
+                '👎' => tkn.kind = TokenType::FALSE,
+                '☯' => tkn.kind = TokenType::BOOL,
+                '🔢' => tkn.kind = TokenType::INT,
+                '🔤' => tkn.kind = TokenType::STR,
+                '🔚' => tkn.kind = TokenType::LEND,
+                '🔁' => tkn.kind = TokenType::WHILE,
+                '📄' => tkn.kind = TokenType::PRINT,
+                '📞' => tkn.kind = TokenType::CALL,
+                _ => {
+                    // Handle variable names
+                    if self.state == TokenizerState::DEFINING {
+                        tkn.kind = TokenType::ID;
+                        let (end_pos, id) = self.create_id(pos);
+                        pos = end_pos;
+                        tkn.value_str = id;
+                        self.state = TokenizerState::NONE;
+                    } else { // Handle value creation
+                        tkn.kind = TokenType::VAL;
+
+                        // Make make digit (each keycap is 3 codepoints wide)
+                        if pos + 3 < prog.len() && is_keycap(&prog[pos..pos+3]) {
+                            while pos + 3 < prog.len() && is_keycap(&prog[pos..pos+3]) {
+                                tkn.value_int *= 10;
+                                tkn.value_int += get_keycap_val(prog[pos]);
+                                pos += 3;
+                            }
+                            pos -= 1;
+                        } else { // Handle make string
+                            while pos < prog.len() && !prog[pos].is_whitespace() {
+                                tkn.value_str.push(prog[pos]);
+                                pos += 1;
+                            }
+                            pos -= 1;
+                        }
+                    }
+                }
+            };
+
+            self.pos = pos + 1;
+        } else {
+            tkn.kind = TokenType::END;
+        }
+
+        self.normalize_pos_and_state(tkn.kind);
+
+        return tkn;
+    }
+
+    fn normalize_pos_and_state(&mut self, kind: TokenType) {
+        if kind == TokenType::EQ || kind == TokenType::EQEQ || kind == TokenType::MUL || kind == TokenType::BOOL {
+            self.pos += 1;
+        }
+
+        if kind == TokenType::BOOL || kind == TokenType::INT || kind == TokenType::STR {
+            self.state = TokenizerState::DEFINING;
+        }
+    }
+
+    fn fast_forward_whitespace(&mut self, start: usize) -> usize {
+        let mut i = start;
+        let prog = &self.program;
+        while i < prog.len() && prog[i].is_whitespace() {
+            i += 1;
+        }
+
+        return i;
+    }
+
+    fn create_id(&self, mut start: usize) -> (usize, String) {
+        let mut id = String::new();
+        while {
+            id.push(self.program[start]);
+            start += 1;
+
+            is_emoji(self.program[start])
+        } {}
+
+        (start, id)
+    }
+}
